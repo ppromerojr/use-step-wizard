824b7c920e0b0badb7845e0ece75a787d5600482
Pabs Romero Jr. <ppromerojr@gmail.com>
2026-07-07 20:40:54 +0800

    Update package dependencies and add unit tests for Wizard component and hooks

--- src/hooks/__tests__/useWizardState.test.ts
+++ src/hooks/__tests__/useWizardState.test.ts
@@ -1,7 +1,7 @@
 import { act, renderHook } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 
-import useWizardState from "./useWizardState";
+import useWizardState from "../useWizardState";
 
 describe("useWizardState", () => {
   it("returns default state", () => {
